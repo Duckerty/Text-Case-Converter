f5b04c3ca7f04e005752dbd2c867fec33fd041ea
Duckerty <Pahan14420@gmail.com>
2022-07-06 11:57:54 +0300

    first version of my case converter

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let upperCaseButton = document.getElementById('upper-case');
+let lowerCaseButton = document.getElementById('lower-case');
+let properCaseButton = document.getElementById('proper-case');
+let sentenceCaseButton = document.getElementById('sentence-case');
+let textArea = document.querySelector('textarea');
+
+upperCaseButton.addEventListener('click', function() {
+  let text = textArea.value;
+  textArea.value = text.toUpperCase();
+})
+
+lowerCaseButton.addEventListener('click', function() {
+  let text = textArea.value;
+  textArea.value = text.toLowerCase();
+})
+
+properCaseButton.addEventListener('click', function() {
+  let text = textArea.value;
+  textArea.value = text.split(' ').map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(' ');
+})
+
+sentenceCaseButton.addEventListener('click', function() {
+  let text = textArea.value;
+  textArea.value = text.split('. ').map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join('. ');
+})
+
+function download(filename, text) {
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+document.getElementById("save-text-file").addEventListener("click", function() {
+  let text = textArea.value;
+  let filename = "text.txt";
+  download(filename, text);
+}, false);
